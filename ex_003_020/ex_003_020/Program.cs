b8d520295417bb4e32ee91ead0cd699e61925204
찬명 박 <pcm0028@naver.com>
2020-03-09 19:36:27 +0900

    008 ~ 010 문제풀이

--- ex_003_020/ex_003_020/Program.cs
+++ ex_003_020/ex_003_020/Program.cs
@@ -63,7 +63,43 @@ namespace ex_003_020
              *  008
              */
 
-            
+            //Console.WriteLine("10 이하의 소수 : {0}, {1}, {2}, {3}", 2, 3, 5, 7);
+
+            //string primes;
+            //primes = String.Format("10 이하의 소수 : {0}, {1}, {2}, {3}", 2, 3, 5, 7);
+            //Console.WriteLine(primes);
+
+            /*
+            *  009
+            */
+
+            //int v1 = 100;
+            //double v2 = 1.2345;
+
+            //Console.WriteLine(v1.ToString() + "," + v2.ToString());
+            //Console.WriteLine("v1 = " + v1 + ", v2 = " + v2);
+            //Console.WriteLine("v1 = {0}, v2 = {1}", v1, v2);
+            //Console.WriteLine($"v1 = {v1}, v2 = {v2}");
+
+
+            /*
+             *  010
+             */
+
+            Console.WriteLine("Standard Numberic Format Specifiers");
+
+            Console.WriteLine(
+                "(C)          {0:C}\n" +
+                "(D)          {0:D}\n" +
+                "(E)          {1:E}\n" +
+                "(F)          {1:F}\n" +
+                "(G)          {0:G}\n" +
+                "(N)          {0:N}\n" +
+                "(P)          {1:P}\n" +
+                "(R)          {1:R}\n" +
+                "(X)          {0:X}\n",
+                -123456789, -1234.5678F
+                );
         }
     }
 }
