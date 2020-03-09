4c74294d55e7248804eb353ae1d36f5ac50076fd
찬명 박 <pcm0028@naver.com>
2020-03-09 18:50:56 +0900

    005 ~ 007 문제 풀이

---
+++ ex_003_020/ex_003_020/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+namespace ex_003_020
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             *  005
+             */
+
+            //string a = "hello";
+            //string b = "h";
+
+            //b = b + "ello";
+
+            //Console.WriteLine(a == b);
+            //Console.WriteLine("b = " + b);
+
+            //int x = 10;
+            //string c = b + "!" + " " + x;
+            //Console.WriteLine("c = " + c);
+
+
+            /*
+             *  006
+             */
+
+            //int i;
+            //double x;
+
+            //i = 5;
+            //x = 3.141592;
+            //Console.WriteLine("i = " + i + ", x =" + x);
+
+            //x = i;
+            //i = (int)x;
+            //Console.WriteLine("i = " + i + ", x =" + x);
+
+
+            /*
+             *  007
+             */
+
+            //bool b = true;
+            //char c = 'a';
+            //decimal d = 1.234m;
+            //double e = 1.23456789;
+            //float f = 1.23456789f;
+            //int i = 1234;
+            //string s = "hello";
+
+            //Console.WriteLine(b);
+            //Console.WriteLine(c);
+            //Console.WriteLine(d);
+            //Console.WriteLine(e);
+            //Console.WriteLine(f);
+            //Console.WriteLine(i);
+            //Console.WriteLine(s);
+
+            /*
+             *  008
+             */
+
+            
+        }
+    }
+}
